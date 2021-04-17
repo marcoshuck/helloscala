51357e728f9319a6a10e250c83f29c49a7885256
Marcos Huck <marcos@huck.com.ar>
2021-04-16 21:46:42 -0300

    Add squareOf function.

---
+++ src/main/scala/Recursion.scala
@@ -0,0 +1,5 @@
+package ar.com.huck.helloscala
+
+object Recursion {
+  def squareOf(n: Int): Int = n * n
+}
---
+++ src/test/scala/RecursionTestSuite.scala
@@ -0,0 +1,17 @@
+import org.scalatest.FunSuite
+
+class RecursionTestSuite extends FunSuite {
+  import ar.com.huck.helloscala.Recursion.squareOf
+
+  test("squareOf(1) == 1") {
+    assertResult(1)(squareOf(1))
+  }
+
+  test("squareOf(2) == 4") {
+    assertResult(4)(squareOf(2))
+  }
+
+  test("squareOf(4) == 16") {
+    assertResult(16)(squareOf(4))
+  }
+}
