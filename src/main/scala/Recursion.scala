77d0daf6bfae4f94dcd8c3d95e2950f542d6998a
Marcos Huck <marcos@huck.com.ar>
2021-04-16 21:59:11 -0300

    Add factorial function.

--- src/main/scala/Recursion.scala
+++ src/main/scala/Recursion.scala
@@ -4,4 +4,6 @@ object Recursion {
   def squareOf(n: Int): Int = n * n
 
   def pow(v: Long, n: Int): Long = if (n == 0) 1 else v * pow(v, n-1)
+
+  def factorial(f: Long): Long = if (f == 0) 1 else f * factorial(f - 1)
 }
